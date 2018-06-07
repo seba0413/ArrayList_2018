d3eab82d8cdeb17e51b4a740ae136db20fbe2138
marinaAC <maru.cardozo02@gmail.com>
2018-06-06 21:47:48 -0300

    Version con los primeros dos test funcionando

--- arraylist/examples/example_4/src/ArrayList.c
+++ arraylist/examples/example_4/src/ArrayList.c
@@ -71,6 +71,27 @@ int al_add(ArrayList* this, void* pElement)
 {
     int returnAux = -1;
 
+    if(this != NULL && pElement != NULL)
+    {
+        if (this->size < this->reservedSize)
+        {
+            this->pElements[this->size] = pElement;
+            this->size++;
+            returnAux = 0;
+        }
+        else
+        {
+            if(!resizeUp(this))
+            {
+                this->pElements[this->size] = pElement;
+                this->size++;
+                returnAux = 0;
+            }
+
+        }
+
+    }
+
     return returnAux;
 }
 
@@ -302,6 +323,22 @@ int al_sort(ArrayList* this, int (*pFunc)(void* ,void*), int order)
 int resizeUp(ArrayList* this)
 {
     int returnAux = -1;
+    void* auxiliar;
+
+    if(this != NULL)
+    {
+        auxiliar = realloc(this->pElements, sizeof(void*)*(this->reservedSize + AL_INCREMENT));
+        if(auxiliar != NULL)
+        {
+            this->pElements = auxiliar;
+            this->reservedSize = this->reservedSize+AL_INCREMENT;
+            returnAux = 0;
+
+        }
+
+
+    }
+
 
     return returnAux;
 
--- arraylist/examples/example_4/src/main.c
+++ arraylist/examples/example_4/src/main.c
@@ -30,7 +30,7 @@ int main(void)
 {
         startTesting(1);
         startTesting(2);
-        startTesting(3);
+        /*startTesting(3);
         startTesting(4);
         startTesting(5);
         startTesting(6);
@@ -44,7 +44,7 @@ int main(void)
         startTesting(14);
         startTesting(15);
         startTesting(16);
-        startTesting(17);
+        startTesting(17);*/
 
     return 0;
 }
